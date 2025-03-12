517c95939a739a8f9ae5390e1e3582f542c1893e
igorfcfs <igorfcfs@gmail.com>
2025-03-11 22:18:01 -0300

    File System (Sync & Async) e HTTP Module

--- app.js
+++ app.js
@@ -2,17 +2,20 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     if (req.url === '/'){
-        res.end('Welcome')
+        return res.end('Welcome')
+    } else if (req.url === '/about'){
+        return res.end('Hi there')
     }
-    if (req.url === '/about'){
-        res.end('Hi there')
-    }
-    
-    res.end(`
+
+    // Se não for nenhuma das rotas acima, retorna erro 404
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    return res.end(`
     <h1>Oops!</h1>
     <p>We can't seem to find the page you are looking for</p>
     <a href="/">back home</a>
     `);
 })
 
-server.listen(5000);
+server.listen(5000, () => {
+    console.log('Servidor rodando em http://localhost:5000');
+});
